713197f736d3309a28b50dc6120707b1e788b294
Yerzhan Ashen <ashenov.e@gmail.com>
2026-04-13 13:59:40 +0100

    update all project

---
+++ app/Http/Middleware/SetLocale.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Session;
+use Symfony\Component\HttpFoundation\Response;
+
+class SetLocale
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($request->has('lang')) {
+            $lang = $request->get('lang');
+            if (in_array($lang, ['en', 'ru', 'kz'])) {
+                Session::put('locale', $lang);
+            }
+        }
+
+        $locale = Session::get('locale', config('app.locale'));
+        App::setLocale($locale);
+
+        return $next($request);
+    }
+}
