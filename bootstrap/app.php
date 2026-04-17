80ca57e5c3fc787c755fce9a1eb601457119acdd
Yerzhan Ashen <ashenov.e@gmail.com>
2026-04-17 09:05:55 +0100

    Add CSRF exception for BCC routes in middleware configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->preventRequestForgery(except: [
+            'bcc/*',
+        ]);
         $middleware->web(append: [
             SetLocale::class,
         ]);
